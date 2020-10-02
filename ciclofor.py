1ca850ee55c278f2957d20fbda99e1118bd552fe
Byron Acosta <acostatorres.by@gmail.com>
2020-10-02 13:24:16 -0300

    se agrego ciclo for en python

---
+++ ciclofor.py
@@ -0,0 +1,15 @@
+#crear cilco for que permita opara ingresar para n numeros, donde n es un numero ingresado por teclado
+#calcular y mostrar cantidad de numeros pares y cantidad de numeros impares
+
+veces=int(input("Cuantos números ingresa?: "))
+par=0
+impar=0
+for x in range(veces):
+    nume=int(input("Ingrese un número: "))
+    if (nume%2==0):
+        par=par+1
+    elif(nume%2!=0):
+        impar=impar+1
+
+print("La cantidad de números pares es: " + str(par))
+print("La cantidad de números impares es: " + str(impar))
